eeac0cf758a17ce389b07f675953f45bc1e7f36b
pione30 <g.pionelish30@gmail.com>
2019-09-16 03:00:40 +0900

    creating payroll

---
+++ src/exercise3.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+pub fn add_an_employee(payroll: &mut HashMap<String, Vec<String>>, query: String) -> Result<&mut HashMap<String, Vec<String>>, String> {
+    let split_query: Vec<&str> = query.split(' ').collect();
+
+    if split_query.len() < 4 || split_query[0] != "Add" || split_query[2] != "to" {
+        return Err("Query must be like 'Add <Name> to <Department>'".to_string());
+    }
+
+    payroll.entry(split_query[3].to_string()).or_insert(Vec::new()).push(split_query[1].to_string());
+
+    Ok(payroll)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod exercise1;
 mod exercise2;
+mod exercise3;
+
+use std::collections::HashMap;
 
 fn main() {
     let mut test_vec: Vec<usize> = vec![1, 2, 3, 5, 2, 9, 42];
@@ -13,4 +16,8 @@ fn main() {
     assert_eq!(exercise2::to_pig_latin(&"Grüße, Jürgen ❤".to_string()), None);
     assert_eq!(exercise2::to_pig_latin(&"s".to_string()), None);
     assert_eq!(exercise2::to_pig_latin(&"".to_string()), None);
+
+    let mut payroll = HashMap::new();
+    let payroll = exercise3::add_an_employee(&mut payroll, "Add Sally to Engineering".to_string());
+    assert_eq!(payroll.ok().unwrap()["Engineering"], vec!["Sally"]);
 }
